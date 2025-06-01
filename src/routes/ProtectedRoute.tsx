979e0262e6b7f89119f50753d896c2698cd4b486
biswajit2k24-2 <biswajit.qwegle@gmail.com>
2025-06-01 15:32:48 +0530

    role based

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,10 +1,33 @@
-import { Navigate, Outlet } from 'react-router-dom';
-// import { useAuth } from '../config/authContext';
+// import { Navigate, Outlet } from 'react-router-dom';
+// // import { useAuth } from '../config/authContext';
 
-const ProtectedRoute = () => {
-  // const { user } = useAuth();
+// const ProtectedRoute = () => {
+//   // const { user } = useAuth();
+//   const id = sessionStorage.getItem('userID');
+//   return id ? <Outlet /> : <Navigate to="/login" replace />;
+// };
+
+// export default ProtectedRoute;
+
+
+
+
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+const ProtectedRoute = ({ requiredRole }:any) => {
   const id = sessionStorage.getItem('userID');
-  return id ? <Outlet /> : <Navigate to="/login" replace />;
+  const role = sessionStorage.getItem('role'); // make sure you store this at login
+  const location = useLocation();
+
+  if (!id) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
+  if (requiredRole && role !== requiredRole) {
+    return <Navigate to="*" replace />;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
--- src/routes/RoleBasedRedirect.tsx
+++ src/routes/RoleBasedRedirect.tsx
@@ -1,15 +1,38 @@
+// import { Navigate } from 'react-router-dom';
+// import { paths } from './Path';
+// // import { useAuth } from '../config/authContext';
+
+// const RoleBasedRedirect = () => {
+//   const role = sessionStorage.getItem('role');
+//   //  const { user } = useAuth();
+
+//   if (role === 'ADMIN') return <Navigate to={paths.adminDashboard} replace />;
+//   if (role === 'USER') return <Navigate to={paths.userDashboard} replace />;
+
+//   return <Navigate to="/login" replace />;
+// };
+
+// export default RoleBasedRedirect;
+
+
+import React from 'react'
 import { Navigate } from 'react-router-dom';
 import { paths } from './Path';
-// import { useAuth } from '../config/authContext';
+import { toast } from 'react-toastify';
 
-const RoleBasedRedirect = () => {
-  const role = sessionStorage.getItem('role');
-  //  const { user } = useAuth();
-
-  if (role === 'ADMIN') return <Navigate to={paths.adminDashboard} replace />;
-  if (role === 'USER') return <Navigate to={paths.userDashboard} replace />;
-
-  return <Navigate to="/login" replace />;
+const RoleBasedRedirect:React.FC = () => {
+  const ROLES = {
+  ADMIN: 'ADMIN',
+  USER: 'USER',
 };
+  const role = sessionStorage.getItem('role')?.toUpperCase();
 
-export default RoleBasedRedirect;
+ if (role === ROLES.ADMIN) return <Navigate to={paths.adminDashboard} replace />;
+if (role === ROLES.USER) return <Navigate to={paths.userDashboard} replace />;
+
+
+toast.error(`Unknown role: ${role}`);
+return <Navigate to="*" replace />;
+}
+
+export default RoleBasedRedirect
